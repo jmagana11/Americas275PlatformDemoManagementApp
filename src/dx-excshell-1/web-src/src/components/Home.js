0400c8c0a68ca8b5c25a033f846ee1244dded778
Juan Magana <jmagana@juans-mbp.lan>
2023-02-10 15:13:24 -0500

    first commit

---
+++ src/dx-excshell-1/web-src/src/components/Home.js
@@ -0,0 +1,14 @@
+/*
+* <license header>
+*/
+
+import React from 'react'
+import { Heading, View, Text } from '@adobe/react-spectrum'
+export const Home = () => (
+  <View width='size-6000'>
+    <Heading level={1}>Welcome to Americas275 Environment Management App!</Heading>
+    <Text> This app will grow with more services from our HOL POSTMAN Collections.</Text>
+    <Text> The idea is to have an application that allows us to get more done.</Text>
+    <Text> This first version only supports segment refresh from any sandbox; We will be adding more services soon.</Text>
+  </View>
+)
---
+++ src/dx-excshell-1/web-src/src/components/SegmentRefresh.js
@@ -0,0 +1,166 @@
+import React, { useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
+import {
+    Flex,
+    Heading,
+    Form,
+    Picker,
+    TextArea,
+    ActionButton,
+    StatusLight,
+    ProgressBar,
+    Item,
+    Text,
+    View
+} from '@adobe/react-spectrum'
+import Function from '@spectrum-icons/workflow/Function'
+import allActions from '../config.json'
+
+function SegmentRefresh(props) {
+    const [firstPickerItems, setFirstPickerItems] = useState([]);
+    const [secondPickerItems, setSecondPickerItems] = useState([]);
+    const [selectedFirstPickerItem, setSelectedFirstPickerItem] = useState(null);
+    const [selectedSecondPickerItem, setSelectedSecondPickerItem] = useState(null);
+    const [refreshedSegmentJobItem, setRefreshedSegmentJobItem] = useState(null);
+    const [segmentStatusItem, setSegmentStatusItem] = useState(null);
+    const [isJobLoading, setIsJobLoading] = useState(false);
+    const [isStatusLoading, setIsStatusLoading] = useState(false);
+
+    const actionHeaders = {
+        'Content-Type': 'application/json'
+    }
+
+    const fetchConfig = {
+        headers: actionHeaders
+    }
+
+    if (window.location.hostname === 'localhost') {
+        actionHeaders['x-ow-extra-logging'] = 'on'
+    }
+
+    // set the authorization header and org from the ims props object
+    if (props.ims.token && !actionHeaders.authorization) {
+        actionHeaders.authorization = `Bearer ${props.ims.token}`
+    }
+    if (props.ims.org && !actionHeaders['x-gw-ims-org-id']) {
+        actionHeaders['x-gw-ims-org-id'] = props.ims.org
+    }
+
+    useEffect(() => {
+        fetch(allActions.getsandboxes, fetchConfig)
+            .then((response) => response.json())
+            .then((data) => {
+                const options = data.sandboxes.map((item) => ({
+                    name: item.name,
+                    value: item.eTag
+                }));
+                setFirstPickerItems(options);
+            });
+    }, []);
+
+    useEffect(() => {
+        if (!selectedFirstPickerItem) {
+            return;
+        }
+        console.log("secondPicker FirstItem selected", selectedFirstPickerItem)
+        actionHeaders['sandboxName'] = selectedFirstPickerItem;
+        fetch(allActions.getSegments, fetchConfig)
+            .then((response) => response.json())
+            .then((data) => {
+                const options = data.segments.map((item) => ({
+                    name: item.name,
+                    value: item.id
+                }));
+                console.log(JSON.stringify(options));
+                setSecondPickerItems(options);
+            });
+
+    }, [selectedFirstPickerItem]);
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log('handleSubmit/selectedSecondPickerITem',selectedSecondPickerItem)
+        actionHeaders['sandboxName'] = selectedFirstPickerItem;
+        actionHeaders['segmentId'] = selectedSecondPickerItem;
+        setIsJobLoading(true)
+        fetch(allActions.refreshSegment, fetchConfig)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('Form submitted successfully:', data);
+                setRefreshedSegmentJobItem(data.id);
+                if(data){setIsJobLoading(false);}
+            });
+    }
+
+    function handleSubmitSegmentRef(event){
+        event.preventDefault();
+        setIsStatusLoading(true)
+        console.log('handleSubmit/refreshedSegmentJobItem',refreshedSegmentJobItem)
+        actionHeaders['sandboxName'] = selectedFirstPickerItem;
+        actionHeaders['segmentId'] = selectedSecondPickerItem;
+        actionHeaders['jobId'] = refreshedSegmentJobItem;
+
+        fetch(allActions.getSegmentJobStatus, fetchConfig)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('Form submitted successfully:', data);
+                if(data){setIsStatusLoading(false);}
+                setSegmentStatusItem(data);
+            })
+
+    }
+
+
+    return (
+        <View width="size-6000">
+            <Heading>Please Select a Sandbox, then a Segment. Stay on this page to keep getting status. If you close the browser tab or navigate to other parts within the app, you'll lose your changes</Heading>
+            <Form onSubmit={handleSubmit}>
+                <Picker
+                    label="Sandbox Picker:"
+                    isRequired={true}
+                    placeholder="Select an item"
+                    aria-label="Select an item for the first picker"
+                    items={firstPickerItems}
+                    itemKey="name"
+                    onSelectionChange={(name) => {
+                        console.log(`value: ${name}`)
+                        setSelectedFirstPickerItem(name);
+                    }}>
+                    {(item) => <Item key={item.name}>{item.name}</Item>}
+                </Picker>
+                {console.log("selectedFirstPickerItem", selectedFirstPickerItem)}
+                {selectedFirstPickerItem && ( <Picker
+                    label="Second Picker"
+                    placeholder="Select an item"
+                    aria-label="Select anitem for the first picker"
+                    items={secondPickerItems}
+                    itemKey="value"
+                    onSelectionChange={(value) => { setSelectedSecondPickerItem(value); }}>
+                    {(item) => <Item key={item.value}>{item.name}</Item>}
+                </Picker>)}
+                <Flex><ActionButton disabled={isJobLoading} type="submit">Submit</ActionButton></Flex>
+                {isJobLoading && ( <ProgressBar 
+                                aria-label="Loading.."
+                                isIndeterminate={true}/>) }
+            </Form>
+            {refreshedSegmentJobItem && ( 
+                <div>
+                <Heading>{`Segment Job ID: ${refreshedSegmentJobItem}`}</Heading>
+                <Form onSubmit={handleSubmitSegmentRef}>
+                    <Flex><ActionButton disabled={isStatusLoading} type="submit">Get Job Status</ActionButton></Flex>
+                </Form></div>)}
+                {isStatusLoading && ( <ProgressBar 
+                                aria-label="Loading.."
+                                isIndeterminate={true}/>) }
+            {segmentStatusItem && (<div><Heading>{`Status: ${segmentStatusItem.status}`}</Heading></div>)}
+        </View>
+    );
+}
+
+SegmentRefresh.propTypes = {
+    runtime: PropTypes.any,
+    ims: PropTypes.any
+}
+
+export default SegmentRefresh
+
